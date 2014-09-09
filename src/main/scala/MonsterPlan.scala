e59bac2e96037f6c75ba91583b81d072d5015789
eirmelan <eirik.meland@finn.no>
2014-09-09 11:07:58 +0200

    echo server up and running

--- src/main/scala/MonsterPlan.scala
+++ src/main/scala/MonsterPlan.scala
@@ -1,7 +1,12 @@
 import unfiltered.request._
 import unfiltered.response._
 
-class MonsterPlan {
+object MonsterShop extends App {
 
+  val echo = unfiltered.filter.Planify {
+    case Path(Seg(p :: Nil)) => ResponseString(p)
+  }
 
+  unfiltered.jetty.Server.anylocal.plan(echo).run()
 }
+
