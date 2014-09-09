7e10c3587d7657dfc71852132ae50b81afc779a4
jarnyste <jari.nystedt@finn.no>
2014-09-09 11:34:56 +0200

    urls

--- src/main/scala/MonsterPlan.scala
+++ src/main/scala/MonsterPlan.scala
@@ -10,3 +10,22 @@ object MonsterShop extends App {
   unfiltered.jetty.Server.anylocal.plan(echo).run()
 }
 
+
+import linx._
+object urls {
+  //basketService
+  val service = Root / "service"
+  val basket = Root / "service" / "basket"
+  val basketPost = basket / 'id
+  val basketSump = basket / "sum"
+  // orderService
+  val orders = service / "orders"
+  val order = orders / 'aggregateId
+
+  // authService
+  val authLogin = service / "auth" / "logIn"
+  val authLogout = service / "auth" / "logOut"
+  val authCustomer = service / "auth" / "customer"
+  // monsterService
+  val monsterTypes = service / "monsterTypes"
+}
